ab230794d6eb623f77720da41ac73a1521372ce1
B1gDaddyKane <jenschrj97@gmail.com>
2022-12-01 23:54:13 +0100

    Task b for the first day of advent of code done

--- Day_1/b.ts
+++ Day_1/b.ts
@@ -3,13 +3,16 @@ const brackets = input.split("\r\n");
 
 let largestCal = 0;
 let tempCal = 0;
+const totalCals: number[] = [];
 
 brackets.forEach((b, i)  => {
     const nextWord = brackets[i + 1] == null ? brackets[i] : brackets[i + 1];
     tempCal = tempCal + +b;
     if (!nextWord.length || nextWord == brackets[i]) {
-        largestCal = tempCal > largestCal ? tempCal : largestCal;
+        totalCals.push(tempCal);
         tempCal = 0;
     }
 })
-console.log("largestCal: ", largestCal)
+totalCals.sort((a, b) => a - b).reverse().slice(0,3).forEach(c => largestCal += c);
+
+console.log(largestCal);
