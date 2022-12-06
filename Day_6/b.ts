cc9fa3ad12291a7940dc621d86c8a145ac63bc5d
B1gDaddyKane <jenschrj97@gmail.com>
2022-12-06 21:06:02 +0100

    Both task A and B for day 6 of Advent of Code year 2022

---
+++ Day_6/b.ts
@@ -0,0 +1,22 @@
+const input = await Deno.readTextFile("Day_6/input.txt");
+
+const stream = input.split("");
+let marker : string[] = [];
+let appearedAt = 0;
+
+stream.forEach((s, index) => {
+    if(appearedAt == 0){
+        if(marker.length == 14) {
+            appearedAt = index;
+            return;
+        }
+        if(marker.includes(s)) {
+            marker = marker.slice(marker.indexOf(s) + 1, marker.length)
+            marker.push(s);
+            return;
+        }
+        marker.push(s);
+    }
+});
+
+console.log("The marker:", marker,"appeared after:",appearedAt,"characters")
