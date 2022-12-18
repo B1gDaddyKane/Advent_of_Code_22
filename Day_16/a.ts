d2b9e5cdbfa6b7b4088dfc18b761ffdaba8681f9
B1gDaddyKane <jenschrj97@gmail.com>
2022-12-18 21:32:31 +0100

    Day_16 not done

--- Day_16/a.ts
+++ Day_16/a.ts
@@ -3,22 +3,54 @@ const input = await Deno.readTextFile("Day_16/test.txt");
 type Valves = {
     name: string;
     flowRate: number;
-    tunnels: Valves[];
+    tunnels: string[];
 }
 
 const createValves = (input: string): Valves => {
     const valve: Valves = {} as Valves;
-    valve.flowRate = +input.split("").filter(i => +i)[0];
+    valve.flowRate = +input.split("").filter(i => +i).join("");
     valve.name = input.split(" ")[1];
-    console.log(input.match(/\b\w{2}\b/))
-    valve.tunnels = input.match(/\b[A-Z]{2}\b/)?.filter(i => i !== valve.name).map(i => createValves(i) as Valves)!;
+    valve.tunnels = input.split(" ").filter(i => i.match(/\b[A-Z]{2}\b/)).filter(i => i !== valve.name).map(i => i.replace(",", ""));
     return valve;
 }
 
 const valves = input.split("\r\n").map(i => createValves(i) as Valves)
 
-let time = 30;
 let totalPressureReleased = 0;
 const openedValves: Valves[] = [];
+let currentValve: Valves = valves[0];
 
-console.log(valves);
+for (let index = 30; index >= 0; index--) {
+    let nextValve: Valves = {flowRate: 0} as Valves;
+
+    openedValves.forEach(o => {
+        totalPressureReleased += o.flowRate;
+    });
+
+    if(currentValve.flowRate != 0 && !openedValves.includes(currentValve)) {
+        index--;
+
+        openedValves.push(currentValve);
+
+        currentValve.tunnels.forEach(t => {
+            nextValve =  valves.filter(v => v.name === t)[0].flowRate >= nextValve.flowRate 
+            && !openedValves.includes(valves.filter(v => v.name === t)[0]) 
+            ? valves.filter(v => v.name === t)[0] 
+            : nextValve;
+        });
+
+        currentValve = nextValve;
+        continue;
+    }
+
+    currentValve.tunnels.forEach(t => {
+        nextValve =  valves.filter(v => v.name === t)[0].flowRate >= nextValve.flowRate 
+        && !openedValves.includes(valves.filter(v => v.name === t)[0])
+        ? valves.filter(v => v.name === t)[0] 
+        : nextValve;
+    });
+
+    currentValve = nextValve;
+    continue
+}
+console.log(totalPressureReleased);
