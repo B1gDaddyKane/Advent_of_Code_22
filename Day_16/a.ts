cd1d9710f3952302655b17117c488341351f2e9b
Jens Jensen <jcj@it-minds.dk>
2022-12-20 15:14:37 +0100

    Added bfs and some changes to the valve type

--- Day_16/a.ts
+++ Day_16/a.ts
@@ -1,56 +1,54 @@
 const input = await Deno.readTextFile("Day_16/test.txt");
 
 type Valves = {
-    name: string;
     flowRate: number;
-    tunnels: string[];
+    tunnelsDistances: ValveDistances;
+    name: string
+}
+
+type ValveDistances = {
+    [name: string]: number
 }
 
 const createValves = (input: string): Valves => {
     const valve: Valves = {} as Valves;
-    valve.flowRate = +input.split("").filter(i => +i).join("");
-    valve.name = input.split(" ")[1];
-    valve.tunnels = input.split(" ").filter(i => i.match(/\b[A-Z]{2}\b/)).filter(i => i !== valve.name).map(i => i.replace(",", ""));
+    const inputSplit = input.replace(/,/g,"").split(" ");
+    valve.name = inputSplit[1];
+    valve.flowRate = parseInt(inputSplit[4].replace(";","").split("=")[1]);
+    valve.tunnelsDistances = {} as ValveDistances;
+    inputSplit.slice(9).forEach(i => {
+        valve.tunnelsDistances[i] = 0;
+    });
     return valve;
 }
 
 const valves = input.split("\r\n").map(i => createValves(i) as Valves)
 
-let totalPressureReleased = 0;
-const openedValves: Valves[] = [];
-let currentValve: Valves = valves[0];
+const bfs = (start: string, end: string) : string[] => {
+    const queue: string[][] = [];
+    const visited: string[] = [start];
 
-for (let index = 30; index >= 0; index--) {
-    let nextValve: Valves = {flowRate: 0} as Valves;
+    if (start == end) return [start];
+    queue.push([start]);
+    
+    while(queue.length > 0) {
+        const path: string[] = queue.shift()!;
+        const node:string = path[path.length - 1];
 
-    openedValves.forEach(o => {
-        totalPressureReleased += o.flowRate;
-    });
+        for (const neighbor in valves.find(v => v.name == node)!.tunnelsDistances) {
+            if (visited.includes(neighbor)) continue;
 
-    if(currentValve.flowRate != 0 && !openedValves.includes(currentValve)) {
-        index--;
-
-        openedValves.push(currentValve);
-
-        currentValve.tunnels.forEach(t => {
-            nextValve =  valves.filter(v => v.name === t)[0].flowRate >= nextValve.flowRate 
-            && !openedValves.includes(valves.filter(v => v.name === t)[0]) 
-            ? valves.filter(v => v.name === t)[0] 
-            : nextValve;
-        });
-
-        currentValve = nextValve;
-        continue;
+            if(neighbor == end) return path.concat([neighbor]);
+            visited.push(neighbor);
+            queue.push(path.concat([neighbor]));
+        }
     }
-
-    currentValve.tunnels.forEach(t => {
-        nextValve =  valves.filter(v => v.name === t)[0].flowRate >= nextValve.flowRate 
-        && !openedValves.includes(valves.filter(v => v.name === t)[0])
-        ? valves.filter(v => v.name === t)[0] 
-        : nextValve;
-    });
-
-    currentValve = nextValve;
-    continue
+    return [];
 }
-console.log(totalPressureReleased);
+
+valves.forEach(start => {
+    valves.forEach(end => {
+        start.tunnelsDistances[end.name] = bfs(start.name, end.name).length - 1;
+    });
+});
+console.log(valves);
